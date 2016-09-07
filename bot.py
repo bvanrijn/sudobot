f1401c16bb6d1ed6e6e2467c0d4364d28025cb17
Barend <bvanrijn@users.noreply.github.com>
2016-09-07 15:22:28 +0200

    added inline queries but it does not work yet

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import configparser
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import ChatAction
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
+from telegram import InlineQueryResultArticle, ChatAction, InputTextMessageContent
+from uuid import uuid4
 import subprocess
 import time
 import logging
@@ -61,16 +62,34 @@ def start(bot, update):
                         text="Oh, before I forget: sudobot is open source software. See my [GitHub repo](https://github.com/bvanrijn/sudobot).", parse_mode="Markdown")
 
 
-def execute(bot, update):
-    if update.message.from_user.id == int(config['ADMIN']['id']):
+def execute(bot, update, direct=True):
+    print(update)
+    if update.message.from_user.id or update.from_user.id == int(config['ADMIN']['id']):
         bot.sendChatAction(chat_id=update.message.chat_id,
                            action=ChatAction.TYPING)
         output = subprocess.Popen(
             update.message.text, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = output.stdout.read().decode('utf-8')
         output = '`{0}`'.format(output)
-        bot.sendMessage(chat_id=update.message.chat_id,
+        if direct:
+            bot.sendMessage(chat_id=update.message.chat_id,
                         text=output, parse_mode="Markdown")
+            return False
+        else:
+            print(update)
+            return output
+
+def inlinequery(bot, update):
+    query = update.inline_query.query
+    results = list()
+
+    results.append(InlineQueryResultArticle(id=uuid4(),
+                                            title=query,
+                                            input_message_content=InputTextMessageContent(
+                                                execute(query, update, False),
+                                                parse_mode="Markdown")))
+
+    bot.answerInlineQuery(update.inline_query.id, results=results)
 
 
 start_handler = CommandHandler('start', start)
@@ -78,6 +97,7 @@ execute_handler = MessageHandler([Filters.text], execute)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(execute_handler)
+dispatcher.add_handler(InlineQueryHandler(inlinequery))
 
 dispatcher.add_error_handler(error)
 
